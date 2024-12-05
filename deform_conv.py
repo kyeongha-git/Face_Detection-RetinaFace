ba901011de0fcf49f796acd5f9efd0ddf8faa2d8
Kyeong Ha Hwang <168719498+kyeongha-git@users.noreply.github.com>
2024-12-05 10:50:56 +0900

    Update deform_conv.py

--- deform_conv.py
+++ deform_conv.py
@@ -5,7 +5,7 @@ from torch import nn
 
 class DeformableConv2d(nn.Module):
     def __init__(self,
-                 in_channels,
+                 in_channels, 
                  out_channels,
                  kernel_size=3,
                  stride=1,
