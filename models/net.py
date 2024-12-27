b33d7483cd7c4e87322f08a2fb93e2a4f145f1b6
Kyeong Ha Hwang <168719498+kyeongha-git@users.noreply.github.com>
2024-12-27 15:18:40 +0900

    Update net.py

--- models/net.py
+++ models/net.py
@@ -1,7 +1,7 @@
 import time
 import torch  
 import torch.nn as nn
-import torchvision.models._utils as _utils
+import torchvision.models._utils as _utils 
 import torchvision.models as models 
 import torch.nn.functional as F  
 from torch.autograd import Variable
